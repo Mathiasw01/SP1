ea8f25102e70383b5550e4678e8c4023db79494b
MathiasW <s224293@student.dtu.dk>
2026-09-02 11:13:57 +0200

    Iteration three

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,79 @@ public class Main {
     static String[] items;
     static int incomingDamage;
 
+    static void printCharacterSheet(){
+        System.out.println("=== CHARACTER SHEET ===");
+        System.out.println("Name: "+ name);
+        System.out.println("Class: "+ type);
+        System.out.println("Level: " +level);
+        System.out.println("Health: " + currentHealth+ "/" + maxHealth);
+        System.out.println("xp: " + exp);
+        System.out.println("Gold: "+ gold);
+        System.out.println("Alive: "+ isAlive);
+        System.out.println("\n\n\n"+"Inventory:" + items.length+":");
+    }
+
+    static void takeDamage(int amount){
+        currentHealth=currentHealth-amount;
+        if (currentHealth<=0){
+            isAlive=false;
+        }
+    }
+    static void heal(int amount){
+        if (currentHealth+amount>=maxHealth){
+            currentHealth=maxHealth;
+        }else {
+            currentHealth=currentHealth+amount;
+        }
+    }
+    void addGold(double amount){
+        gold=gold+amount;
+    }
+    static boolean removeGold(double amount){
+        if (gold>=amount){
+        gold=gold-amount;
+        return true;
+        }else{
+            return false;
+        }
+    }
+    static void addXP(int amount){
+        exp=exp+amount;
+    }
+
+    void levelUp(){
+        level=level+1;
+        maxHealth=maxHealth+5;
+        exp=0;
+    }
+
+    static boolean isHealthCritical(){
+        if (currentHealth<=maxHealth*0.25){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    boolean isAlive(){
+        if (currentHealth>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    static double getHealthPercentage(){
+        int healthPercentage;
+        healthPercentage=currentHealth/(maxHealth/100);
+        return healthPercentage;
+    }
+
+    void printInventory(){
+        for (int i=0;i<items.length;i++){
+            System.out.println("- "+ items[i]);
+        }
+    }
     public static void main(String[] args) {
         name="Ragnar";
         currentHealth=85;
@@ -21,37 +94,26 @@ public class Main {
         type='w';
         incomingDamage=30;
         items= new String[]{"sword", "sheild", "potion"};
-        if (currentHealth<=0){
-            isAlive=false;
-        }
-        System.out.println("=== CHARACTER SHEET ===");
-        System.out.println("Name: "+ name);
-        System.out.println("Class: "+ type);
-        System.out.println("Level: " +level);
-        System.out.println("Health: " + currentHealth+ "/" + maxHealth);
-        System.out.println("xp: " + exp);
-        System.out.println("Gold: "+ gold);
-        System.out.println("Alive: "+ isAlive);
-        System.out.println("\n\n\n"+"Inventory:" + items.length+":");
-        for (int i=0;i<items.length;i++){
-            System.out.println("- "+ items[i]);
-        }
-        System.out.println("\n\n===System===");
-        if (currentHealth<maxHealth*0.25){
-            System.out.println("Critical Warning");
-        }
-        if (exp>=1000*level){
-            System.out.println("Ready to level up");
+        printCharacterSheet();
+
+        takeDamage(30);
+        heal(20);
+        addXP(500);
+
+        if (removeGold(100.0)) {
+            System.out.println("Bought a potion!");
+        } else {
+            System.out.println("Not enough gold!");
         }
 
-        System.out.println("\n\n===Combar===");
-        System.out.println(name+" takes " + incomingDamage+ " damage!");
-        System.out.println("Healt: " + currentHealth+ "->" + (currentHealth-incomingDamage));
-        if (isAlive==true){
-            System.out.println(name+" is alive!");
-        }else {
-            System.out.println(name+" is Dead!");
+        System.out.println("Health: " + getHealthPercentage() + "%");
+
+        if (isHealthCritical()) {
+            System.out.println("WARNING: Find a healer!");
         }
+
+        printCharacterSheet();
+
 
 
 
